28b3a408cd0caa521d92293bc04e2c13d1ca302d
A. Brunner <brunn279@umn.edu>
2026-05-05 18:54:23 -0500

    5/5 1854

--- Final/final_part_d.py
+++ Final/final_part_d.py
@@ -9,11 +9,15 @@ def compZ(zeta):
 def compZeta(a, m, theta):
     return a*np.exp(1j*theta)-m
     #return a*np.cos(theta)+1j*a*np.sin(theta)
-
 def zetafz(z, m):
     return 0.5*(z+np.sqrt(z-2)*np.sqrt(z+2))+m
-def cylFlow(zeta, U, a, m):
+def cylFlow(zeta, U, a):
     return U*zeta+U*a*a/zeta
+def foilFlowAOA(zeta, U, a, aoa):
+    #print(aoa)
+    aoa = aoa*np.pi/180
+    #print(aoa)
+    return U*np.exp(-1j*aoa)*zeta+U*a*a/zeta
 
 x = np.linspace(-3, 3, 1001)
 y = np.linspace(-3, 3, 1001)
@@ -35,11 +39,16 @@ a004 = 1.0 + m004
 a008 = 1.0 + m008
 
 U_inf = 1.0
+aoa = 4.0 #deg
 
-zetzet = zetafz(zz, m008)
-Fzet = cylFlow(zetzet, U_inf, a008, m008)
+zetzet004 = zetafz(zz, m004)
+zetzet008 = zetafz(zz, m008)
+Fzet008 = cylFlow(zetzet008, U_inf, a008)
+print(aoa)
+Fzet004_AOA4 = foilFlowAOA(zetzet004, U_inf, a004, aoa)
+Fzet008_AOA4 = foilFlowAOA(zetzet008, U_inf, a008, aoa)
 
-Zout = compZ(Fzet)
+Zout = compZ(Fzet008)
 
 zeta004 = compZeta(a004,m004,theta)
 z004 = compZ(zeta004)
@@ -104,9 +113,18 @@ axs[1].grid(True)
 
 fig3, ax = plt.subplots()
 
-Contour = ax.contour(xx, yy, Fzet.imag, 30, cmap='plasma')
+Contour = ax.contour(xx, yy, Fzet008.imag, 30, cmap='plasma')
 ax.grid(True)
 
+fig4, axs = plt.subplots(1, 2)
+
+axs[0].contour(xx, yy, Fzet004_AOA4.imag, 100, cmap='plasma')
+axs[0].plot(z004.real, z004.imag,linewidth=2)
+axs[1].contour(xx, yy, Fzet008_AOA4.imag, 100, cmap='plasma')
+axs[1].plot(z008.real, z008.imag,color='black',linewidth=2)
+
+axs[0].grid(True)
+axs[1].grid(True)
 #fig2, axs = plt.subplots(1,2)
 
 #axs[0].plot(theta, zeta.imag)
